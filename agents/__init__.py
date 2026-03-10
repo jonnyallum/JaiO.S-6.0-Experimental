4d1d25a2836c068c7cf7d079a895e6fd1031bfb9
Jonny Allum <jonnyallum@gmail.com>
2026-03-10 19:26:38 +0000

    feat(agents): export all 10 skill nodes in __init__

--- agents/__init__.py
+++ agents/__init__.py
@@ -7,6 +7,11 @@ from agents.security_audit import security_audit_node, SecurityAuditState
 from agents.architecture_review import architecture_review_node, ArchitectureReviewState
 from agents.data_extraction import data_extraction_node, DataExtractionState
 from agents.quality_validation import quality_validation_node, QualityValidationState
+from agents.brief_writer import brief_writer_node, BriefWriterState
+from agents.code_reviewer import code_reviewer_node, CodeReviewState
+from agents.dependency_audit import dependency_audit_node, DependencyAuditState
+from agents.social_post_generator import social_post_generator_node, SocialPostState
+from agents.supabase_intelligence import supabase_intelligence_node, BrainIntelState
 
 __all__ = [
     "github_intelligence_node",
@@ -19,4 +24,14 @@ __all__ = [
     "DataExtractionState",
     "quality_validation_node",
     "QualityValidationState",
+    "brief_writer_node",
+    "BriefWriterState",
+    "code_reviewer_node",
+    "CodeReviewState",
+    "dependency_audit_node",
+    "DependencyAuditState",
+    "social_post_generator_node",
+    "SocialPostState",
+    "supabase_intelligence_node",
+    "BrainIntelState",
 ]
